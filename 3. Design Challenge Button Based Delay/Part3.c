6e4285a74703d0cc618009f94a602f983a8e20a0
Joshua Guillermo <guille42@students.rowan.edu>
2023-02-24 23:19:17 -0500

    Successfully got Part 3 working
    Comments have been updated

--- 3. Design Challenge Button Based Delay/Part3.c
+++ 3. Design Challenge Button Based Delay/Part3.c
@@ -11,7 +11,7 @@ void gpioInit();
 void timerInit();
 
 int ifCount = 0;
-int press = 3000;
+long press = 3000;                          // Original frequency
 
 void main(){
 
@@ -37,11 +37,13 @@ void gpioInit()
           P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
           P2REN |= BIT3;                          // P2.3 pull-up register enable
           P2IES &= ~BIT3;                         // P2.3 Low --> High edge
+          P2IFG &= ~BIT3;
           P2IE |= BIT3;                           // P2.3 interrupt enabled
 
           P4OUT |= BIT1;                          // Configure P4.1 as pulled-up
           P4REN |= BIT1;                          // P4.1 pull-up register enable
           P4IES &= ~BIT1;                         // P4.1 Low --> High edge
+          P4IFG &= ~BIT1;
           P4IE |= BIT1;                           // P4.1 interrupt enabled
 }
 
@@ -49,12 +51,12 @@ void timerInit()
 {
     //Initializing timer for blinking of LED
     TB0CCTL0 = CCIE;                                 // TBCCR0 interrupt enabled
-    TB0CCR0 = press;
+    TB0CCR0 = 3000;
     TB0CTL = TBSSEL_1 | MC_2 | ID_3;                 // ACLK, continuous mode, divided by 8
 
     //Initializing timer for the length of button press
     TB1CCTL0 = CCIE;                          // TBCCR1 interrupt enabled
-    TB1CCR1 = 1;
+    TB1CCR0 = 1;
     TB1CTL = TBSSEL_1 | MC_2 | ID_3;                // ACLK, continuous mode, divided by 8
 
 }
@@ -77,9 +79,9 @@ __interrupt void Port_2(void)
     }
     else if (!(P2IES & BIT3))   // If rising edge (button release)
     {
-        ifCount = 0;            // Button pressed = 0
-        P2IES |= BIT3;          // Look for falling edge
+        ifCount = 0;        // Button pressed = 0
         press = press;
+        P2IES |= BIT3;          // Look for falling edge
     }
 }
 
@@ -89,7 +91,7 @@ __interrupt void Port_4(void)
 {
     P4IFG &= ~BIT1;     // Set interrupt flag to 0
 
-    press = 3000;       // Reset frequency to original
+    press = 500;       // Reset frequency to original
     ifCount = 0;        // Button pressed = 0
 }
 
@@ -102,16 +104,16 @@ __interrupt void Timer0_B0_ISR(void)
 }
 
 // Timer B1 interrupt service routine   (Timer for length of button press)
-#pragma vector = TIMER1_B1_VECTOR
-__interrupt void Timer1_B1_ISR(void)
+#pragma vector = TIMER1_B0_VECTOR
+__interrupt void Timer1_B0_ISR(void)
 {
     if(ifCount == 1)      // If the button is pressed
     {
-        press += 1;          // Add to the press timer
+        press++;        // Add to the press timer
     }
-    else if (ifCount == 0)      // If button is not pressed
+    else     // If button is not pressed
     {
         press = press;
     }
-    //TB1CCR1 += 1;
+    TB1CCR0 += 1;
 }
